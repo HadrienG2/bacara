7ebac7da9feca675ce8c5ab1cacfca361c1c4cc8
Hadrien G <knights_of_ni@gmx.com>
2020-01-24 21:23:16 +0100

    Extract hole allocation into its own function

--- src/lib.rs
+++ src/lib.rs
@@ -370,117 +370,14 @@ impl Allocator {
         let mut hole = hole_opt?;
 
         // Try to allocate the current hole, retry on failure.
-        let first_block_idx = 'alloc_attempts: loop {
-            match hole {
-                // All blocks are in a single superblock, no transaction needed
-                Hole::SingleSuperblock { superblock_idx,
-                                         first_block_subidx } => {
-                    // ...so we just try to allocate
-                    let alloc_result = if num_blocks == BLOCKS_PER_SUPERBLOCK {
-                        debug_assert_eq!(first_block_subidx, 0);
-                        self.try_alloc_superblock(superblock_idx)
-                    } else {
-                        let mask =
-                            SuperblockBitmap::new_mask(first_block_subidx,
-                                                       num_blocks as u32);
-                        self.try_alloc_blocks(superblock_idx, mask)
-                    };
+        let first_block_idx = loop {
+            match self.try_alloc_hole(hole, num_blocks) {
+                Ok(first_block_idx) => break first_block_idx,
 
-                    // Did we succeed?
-                    match alloc_result {
-                        // We managed to allocate this hole
-                        Ok(()) => {
-                            let first_block_idx =
-                                superblock_idx * BLOCKS_PER_SUPERBLOCK
-                                    + (first_block_subidx as usize);
-                            break 'alloc_attempts first_block_idx;
-                        },
-
-                        // We failed to allocate this hole, but we got an
-                        // updated view of the bit pattern for this superblock
-                        Err(observed_bitmap) => {
-                            hole = hole_search.retry(superblock_idx,
-                                                     observed_bitmap)?;
-                            continue 'alloc_attempts;
-                        }
-                    }
-                },
-
-                // Blocks span more than one superblock, need a transaction
-                Hole::MultipleSuperblocks { body_start_idx,
-                                            mut num_head_blocks } => {
-                    // Given the number of head blocks, we can find all other
-                    // parameters of the active transaction.
-                    let other_blocks = num_blocks - num_head_blocks as usize;
-                    let num_body_superblocks =
-                        other_blocks / BLOCKS_PER_SUPERBLOCK;
-                    let mut num_tail_blocks =
-                        (other_blocks % BLOCKS_PER_SUPERBLOCK) as u32;
-
-                    // Try to allocate the body of the transaction
-                    let mut transaction = match AllocTransaction::with_body(
-                        self,
-                        body_start_idx,
-                        num_body_superblocks
-                    ) {
-                        // On success, bubble up the transaction object
-                        Ok(transaction) => transaction,
-
-                        // On failure, send back what went wrong
-                        Err((bad_superblock_idx, observed_bitmap)) => {
-                            hole = hole_search.retry(bad_superblock_idx,
-                                                     observed_bitmap)?;
-                            continue 'alloc_attempts;
-                        },
-                    };
-
-                    // Try to allocate the head of the hole (if any)
-                    while num_head_blocks > 0 {
-                        if let Err(observed_head_blocks) =
-                            transaction.try_alloc_head(num_head_blocks)
-                        {
-                            // On head allocation failure, try to "move the hole
-                            // forward", pushing more blocks to the tail.
-                            num_tail_blocks +=
-                                num_head_blocks - observed_head_blocks;
-                            num_head_blocks = observed_head_blocks;
-                        }
-                    }
-
-                    // If needed, allocate one more body superblock.
-                    // This can happen as a result of moving the hole forward:
-                    //     |0011|1111|1110|0000| -> |0000|1111|1111|1000|
-                    if num_tail_blocks >= BLOCKS_PER_SUPERBLOCK as u32 {
-                        if let Err(observed_bitmap) =
-                            transaction.try_extend_body()
-                        {
-                            hole = hole_search.retry(transaction.body_end_idx(),
-                                                     observed_bitmap)?;
-                            continue 'alloc_attempts;
-                        } else {
-                            num_tail_blocks -= BLOCKS_PER_SUPERBLOCK as u32;
-                        }
-                    }
-
-                    // Try to allocate the tail of the hole (if any)
-                    if num_tail_blocks > 0 {
-                        if let Err(observed_bitmap) =
-                            transaction.try_alloc_tail(num_tail_blocks)
-                        {
-                            hole = hole_search.retry(transaction.body_end_idx(),
-                                                     observed_bitmap)?;
-                            continue 'alloc_attempts;
-                        }
-                    }
-
-                    // We managed to allocate everything! Isn't that right?
-                    debug_assert_eq!(transaction.num_blocks(), num_blocks,
-                                     "Allocated an incorrect number of blocks");
-
-                    // Commit the transaction and get the first block index
-                    let first_block_idx = transaction.commit();
-                    break 'alloc_attempts first_block_idx;
-                },
+                Err((superblock_idx, observed_bitmap)) => {
+                    hole = hole_search.retry(superblock_idx, observed_bitmap)?;
+                    continue;
+                }
             }
         };
 
@@ -655,6 +552,122 @@ impl Allocator {
         self.usage_bitmap[superblock_idx].try_alloc_mask(mask,
                                                          Ordering::Relaxed,
                                                          Ordering::Relaxed)
+    }
+
+    /// Try to allocate from memory found by a HoleSearch
+    ///
+    /// Unlike previous operations, this function is not guaranteed to be atomic
+    /// (other threads may observe the allocation in progress), but it cleans up
+    /// after itself: by the time the function returns, either a suitably sized
+    /// memory region has been allocated, or all allocations have been reverted.
+    ///
+    /// Returns the block index of the start of the allocated region on success,
+    /// and the index of the superblock where things went wrong and the bit
+    /// pattern that was observed there on failure.
+    ///
+    /// This operation has `Relaxed` memory ordering and must be followed by an
+    /// `Acquire` memory barrier in order to avoid allocation being reordered
+    /// after usage of the memory block by the compiler or CPU.
+    pub(crate) fn try_alloc_hole(
+        &self,
+        hole: Hole,
+        num_blocks: usize
+    ) -> Result<usize, (usize, SuperblockBitmap)> {
+        match hole {
+            // All blocks are in a single superblock, no transaction needed
+            Hole::SingleSuperblock { superblock_idx,
+                                     first_block_subidx } => {
+                // ...so we just try to allocate
+                let alloc_result = if num_blocks == BLOCKS_PER_SUPERBLOCK {
+                    debug_assert_eq!(first_block_subidx, 0);
+                    self.try_alloc_superblock(superblock_idx)
+                } else {
+                    let mask =
+                        SuperblockBitmap::new_mask(first_block_subidx,
+                                                   num_blocks as u32);
+                    self.try_alloc_blocks(superblock_idx, mask)
+                };
+
+                // Did we succeed?
+                match alloc_result {
+                    // We managed to allocate this hole
+                    Ok(()) => {
+                        let first_block_idx =
+                            superblock_idx * BLOCKS_PER_SUPERBLOCK
+                                + (first_block_subidx as usize);
+                        Ok(first_block_idx)
+                    },
+
+                    // We failed to allocate this hole, but we got an
+                    // updated view of the bit pattern for this superblock
+                    Err(observed_bitmap) => {
+                        Err((superblock_idx, observed_bitmap))
+                    }
+                }
+            },
+
+            // Blocks span more than one superblock, need a transaction
+            Hole::MultipleSuperblocks { body_start_idx,
+                                        mut num_head_blocks } => {
+                // Given the number of head blocks, we can find all other
+                // parameters of the active transaction.
+                let other_blocks = num_blocks - num_head_blocks as usize;
+                let num_body_superblocks =
+                    other_blocks / BLOCKS_PER_SUPERBLOCK;
+                let mut num_tail_blocks =
+                    (other_blocks % BLOCKS_PER_SUPERBLOCK) as u32;
+
+                // Try to allocate the body of the transaction
+                let mut transaction =
+                    AllocTransaction::with_body(self,
+                                                body_start_idx,
+                                                num_body_superblocks)?;
+
+                // Try to allocate the head of the hole (if any)
+                while num_head_blocks > 0 {
+                    if let Err(observed_head_blocks) =
+                        transaction.try_alloc_head(num_head_blocks)
+                    {
+                        // On head allocation failure, try to "move the hole
+                        // forward", pushing more blocks to the tail.
+                        num_tail_blocks +=
+                            num_head_blocks - observed_head_blocks;
+                        num_head_blocks = observed_head_blocks;
+                    }
+                }
+
+                // If needed, allocate one more body superblock.
+                // This can happen as a result of moving the hole forward:
+                //     |0011|1111|1110|0000| -> |0000|1111|1111|1000|
+                if num_tail_blocks >= BLOCKS_PER_SUPERBLOCK as u32 {
+                    if let Err(observed_bitmap) =
+                        transaction.try_extend_body()
+                    {
+                        return Err((transaction.body_end_idx(),
+                                    observed_bitmap));
+                    } else {
+                        num_tail_blocks -= BLOCKS_PER_SUPERBLOCK as u32;
+                    }
+                }
+
+                // Try to allocate the tail of the hole (if any)
+                if num_tail_blocks > 0 {
+                    if let Err(observed_bitmap) =
+                        transaction.try_alloc_tail(num_tail_blocks)
+                    {
+                        return Err((transaction.body_end_idx(),
+                                    observed_bitmap));
+                    }
+                }
+
+                // We managed to allocate everything! Isn't that right?
+                debug_assert_eq!(transaction.num_blocks(), num_blocks,
+                                 "Allocated an incorrect number of blocks");
+
+                // Commit the transaction and get the first block index
+                Ok(transaction.commit())
+            },
+        }
     }
 
     /// Atomically deallocate a full superblock
